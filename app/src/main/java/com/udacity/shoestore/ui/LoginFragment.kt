5f399ba9d90d1d2c6fd0981ab1f11a821c6a86ff
Ashwani Kumar <ashwanikumar04@gmail.com>
2020-03-15 15:07:23 +0000

    added first

--- app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.udacity.shoestore.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
@@ -16,4 +18,15 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+    override fun onViewCreated(view: View,
+                               savedInstanceState: Bundle?
+    ) {
+
+        btnLogin.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_log_in)
+        }
+    }
+
+
 }
